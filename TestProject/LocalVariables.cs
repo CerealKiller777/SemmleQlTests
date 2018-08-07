951773fff65ba6ec8755965589cdc0fb3a625b18
Michał Nowak <michaln@outlook.com>
2018-08-06 23:52:46 -0700

    Test content.

---
+++ TestProject/LocalVariables.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestProject
+{
+    // ReSharper disable once UnusedMember.Global
+    internal sealed class LocalVariables
+    {
+        // ReSharper disable once UnusedMember.Global
+        public void LocalVariableShouldBeConstant()
+        {
+            // ReSharper disable once ConvertToConstant.Local
+            ulong localVariable = 7;
+            Console.WriteLine(localVariable);
+        }
+    }
+}
---
+++ TestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("52f69f19-535f-4e5b-a0a1-748ca37bf56a")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
